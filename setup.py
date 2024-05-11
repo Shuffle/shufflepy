019bfcc3d219e0690ba6b12f8b4a1f55fff7d2e3
Frikky <frikky@shuffler.io>
2024-05-11 19:05:38 +0200

    Dammit

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='shufflepy',
     version='0.1.0',
-    description='Connect to your favorite services with a single line of code.' 
-    author='shuffle'
+    description='Connect to your favorite services with a single line of code.',
+    author='shuffle',
     author_email='support@shuffler.io',
     packages=find_packages(),
     install_requires=[
