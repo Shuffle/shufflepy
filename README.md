43bd20d28fb5b03e503578c0e6f4d44a6fa59a58
Frikky <frikky@shuffler.io>
2024-05-11 18:45:20 +0200

    Started building out the library

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# shufflepy
-Connect to your favorite services with a single line of code
+# Shufflepy
+Connect to your favorite services with a single line of code. The library does NOT contain any
+
+**This Library is in an Experimental Phase**
+
+## Installation
+```bash
+pip install shufflepy
+```
+
+## Configuration
+```python
+import shufflepy as shuffle
+
+## If config is not specified, the library will use `https://shuffler.io` as the default URL. These are the default values.
+shuffle.config(
+	url='https://shuffler.io',
+	auth='TOKEN',
+)
+```
+
+## Requirements
+- Having an active authentication for the service you are trying to connect to in Shuffle, whether in your own instance or in the Shuffle cloud: [https://shuffler.io](https://shuffler.io/admin?tab=app_auth)
+
+## Usage
+```python
+tickets = shuffle.connect(
+	app='jira', 
+	action='list_tickets',
+)
+
+print(tickets)
+# Returns output according to the 'list_tickets' action
+```
+
+Basic output for `list_tickets`: 
+```
+[{
+  "id": "P-123",
+  "title": "Ticket 1"
+},
+{
+  "id": "P-124",
+  "title": "Ticket 2"
+}]
+```
+
+[See default standards](https://github.com/Shuffle/standards/blob/main/translation_standards) - Standards are modifiable, and you will see your own standards on the [File page in Shuffle](https://shuffler.io/admin?tab=files).
+
+## Available Apps & categories
+- [>2500 APIs: Search in Shuffle](https://shuffler.io/search?tab=apps)
+
+## Available Categories (May 2024)
+- Ticket management 	(Cases)
+- Communication/Email	(Comms)
+- SIEM 					(SIEM)	
+- EDR/XDR 				(Eradication)
+- Asset Management  	(Assets)
+- Identity				(IAM) 
+- Threat Intel			(Intel)
+- Cloud Security		(Cloud)
---
+++ shufflepy/module.py
@@ -0,0 +1,66 @@
+import requests
+
+class Shuffle():
+    def __init__(self, config, verify=False, timeout=10):
+        self.config = config
+        self.verify = verify
+        self.timeout = timeout
+
+    def config(self, url="https://shuffler.io", auth=""):
+        if not url:
+            raise ValueError("url is required")
+
+        if not auth:
+            raise ValueError("auth is required")
+
+        self.config = {
+            "url": url,
+            "auth": auth
+        }
+
+    def get_headers(self):
+        auth = self.config["auth"]
+        if "Bearer" not in auth:
+            auth = f"Bearer {auth}"
+
+        return {
+            "Authorization": self.config["auth"],
+            "User-Agent": "Shufflepy 0.0.1",
+        }
+
+    def connect(self, category="", app="", action="", fields={}):
+        if not category and not app:
+            raise ValueError("category or app is required. Example: app=\"ticket\"")
+
+        if not action:
+            raise ValueError("action is required. Example: action=\"list_tickets\"")
+
+        self.config["category"] = category
+        requestdata = {
+            "action": action,
+        }
+
+        if fields:
+            requestdata["fields"] = fields
+
+        if app:
+            requestdata["app"] = app
+
+        if category:
+            requestdata["category"] = category
+
+        headers = self.get_headers() 
+        response = requests.post(
+            self.config["url"], 
+            json=requestdata, 
+            headers=self.get_headers(),
+            verify=self.verify,
+            timeout=self.timeout,
+        )
+
+        respdata = response.json()
+
+        if response.status_code != 200:
+            raise ValueError(f"Error ({response.status_code}): {respdata}")
+
+        return respdata
