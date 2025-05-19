19ea50b2b5a21c7d8544859a9b03245382c098c6
Frikky <frikky@shuffler.io>
2025-05-20 00:25:04 +0200

    Got it working with shuffle/singul running locally

--- build_shufflepy/setup.py
+++ build_shufflepy/setup.py
@@ -1,19 +1,7 @@
 from setuptools import setup, find_packages
-
-def get_long_description():
-    try:
-        import requests
-        url = "https://raw.githubusercontent.com/shuffle/shufflepy/main/README.md"
-        response = requests.get(url, timeout=5)
-        response.raise_for_status()
-        return response.text
-    except Exception as e:
-        print(f"Warning: failed to fetch remote README.md: {e}")
-        return "Shufflpy package - CLI and library. ERROR: Failed to load README"
-
 setup(
     name='shufflepy',   # Name of the package
-    version='0.1.5',    # Version number
+    version='0.1.6',    # Version number
     description='Use Shufflepy to control Shuffle and Singul', 
     long_description=get_long_description(),  
     long_description_content_type='text/markdown',  
@@ -32,3 +20,15 @@ setup(
     ],
     python_requires='>=3.6',  # Specify Python version requirements
 )
+
+def get_long_description():
+    try:
+        import requests
+        url = "https://raw.githubusercontent.com/shuffle/shufflepy/main/README.md"
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        return response.text
+    except Exception as e:
+        print(f"Warning: failed to fetch remote README.md: {e}")
+        return "Shufflpy package - CLI and library. ERROR: Failed to load README"
+
