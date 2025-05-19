00d808cec79b28456ec0f361838aaea425730622
Frikky <frikky@shuffler.io>
2025-05-19 17:17:42 +0200

    Rebuild again

--- build_shufflepy/setup.py
+++ build_shufflepy/setup.py
@@ -13,7 +13,7 @@ def get_long_description():
 
 setup(
     name='shufflepy',   # Name of the package
-    version='0.1.3',    # Version number
+    version='0.1.4',    # Version number
     description='Use Shufflepy to control Shuffle and Singul', 
     long_description=get_long_description(),  
     long_description_content_type='text/markdown',  
